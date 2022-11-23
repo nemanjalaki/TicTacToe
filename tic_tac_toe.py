e2b4f91b1ef93fbbf60f343aca04b5033524a061
nemanjalaki <sone.lakicevic@gmail.com>
2022-11-23 18:29:36 +0100

    start of tic_tac_toe game

---
+++ tic_tac_toe.py
@@ -0,0 +1,18 @@
+#tabela
+table = [[None, None, None],
+         [None, None, None],
+         [None, None, None]]
+#igrac
+
+player = "x"
+#provjera statusa igre, win loose even
+#moguci nacini za pobjedu
+
+def check_table(table):
+    pass
+    #check by row
+    for item in table:
+        print(item)
+    #check by col
+    #check diagonal
+
