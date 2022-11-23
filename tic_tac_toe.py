5d079b6a641e00c9421d911c6e98cc7843fb8c99
nemanjalaki <sone.lakicevic@gmail.com>
2022-11-23 20:43:28 +0100

    check table logic added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,15 +1,52 @@
 #tabela
-table = [[None, None, None],
-         [None, None, None],
+table = [[1, None, 1],
+         [None, 1, None],
          [None, None, None]]
 #igrac
 
 player = "x"
+status = False
 #provjera statusa igre, win loose even
 #moguci nacini za pobjedu
 
 def check_table(table):
-    pass
+
     #check by row
+    for item in table:
+        if len(set(item)) == 1 and None not in item:
+            status = len(set(item)) == 1
+            # print (f"Row {table.index(item)} {status}")
+            return status
     #check by col
-    #check diagonal
+    j = 0
+    while j<len(table):
+        row=[]
+        for r in range(len(table)):
+            row.append(table[r][j])
+        if len(set(row)) == 1 and None not in row:
+            status = (len(set(row)) == 1)
+            # print (f"Column {str(j)} {status}")
+            return status
+        j+=1
+    #check first diagonal
+    j = 0
+    row = []
+    for item in table:
+        row.append(item[j])
+        j+=1
+    if len(set(row)) == 1 and None not in row:
+        status = (len(set(row)) == 1)
+        # print (f"First diagonal {status}")
+        return status
+    #check opposite diagonal
+    j = len(table[0])-1
+    row = []
+    for item in table:
+        row.append(item[j])
+        j-=1
+    if len(set(row)) == 1 and None not in row:
+        status = (len(set(row)) == 1)
+        # print (f"Opposite diagonal {status}")
+        return status
+
+status = check_table(table)
