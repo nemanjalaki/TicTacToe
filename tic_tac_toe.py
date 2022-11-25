05c43086c29227b80b54d373b746c40664a1eb16
nemanjalaki <sone.lakicevic@gmail.com>
2022-11-25 22:51:03 +0100

    the game is fully function and option for occupied cell is added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,20 +1,19 @@
-#tabela
-table = [[1, 2, 1],
-         [2, 1, 2],
-         [2, 1, 2]]
-#igrac
+#table
+table = [[None, None, None],
+         [None, None, None],
+         [None, None, None]]
 
-player = "x"
+# we start with player X
+player = "X"
+# status of the game will be false until win or even
 status = False
-#provjera statusa igre, win loose even
-#moguci nacini za pobjedu
 
+#check table for win on row, column or diagonal
 def check_table(table):
     #check by row
     for item in table:
         if len(set(item)) == 1 and None not in item:
             status = len(set(item)) == 1
-            # print (f"Row {table.index(item)} {status}")
             return status
     #check by col
     j = 0
@@ -24,7 +23,6 @@ def check_table(table):
             row.append(table[r][j])
         if len(set(row)) == 1 and None not in row:
             status = (len(set(row)) == 1)
-            # print (f"Column {str(j)} {status}")
             return status
         j+=1
     #check first diagonal
@@ -35,7 +33,6 @@ def check_table(table):
         j+=1
     if len(set(row)) == 1 and None not in row:
         status = (len(set(row)) == 1)
-        # print (f"First diagonal {status}")
         return status
     #check opposite diagonal
     j = len(table[0])-1
@@ -45,23 +42,56 @@ def check_table(table):
         j-=1
     if len(set(row)) == 1 and None not in row:
         status = (len(set(row)) == 1)
-        # print (f"Opposite diagonal {status}")
         return status
-
+# check for even result
 def check_if_even(table):
     for item in table:
-        if None not in item:
-            return True
-
+        for el in item:
+            if el!="X" and el!="O":
+                return False
+    return True
+# print table
 def print_table(table):
 
     for item in table:
         print("-"*10)
         print(*item,sep=" | ")
     print("-"*10)
-        
-            
+# spin the game
+while status!=True:
+    print(f"Player {player} on move")
+    row = int(input("Input row: "))
+    column = int(input("Input column: "))
 
+    if table[row][column]!="X" and table[row][column]!="O":
+        table[row][column] = player
+
+        status=check_table(table)
+        if status==True:
+            print(f"Player {player} won")
+        else:
+            status=check_if_even(table)
+            if status==True:
+                print(f"Even")
+        
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+    
+    else:
+        print("Cell occupied!")
+        print(f"Player {player} on move")
+        row = int(input("Input row: "))
+        column = int(input("Input column: "))
+        if table[row][column]!="X" and table[row][column]!="O":
+            table[row][column] = player
+    
+    print_table(table)
+
+
+
+        
 
 
 
